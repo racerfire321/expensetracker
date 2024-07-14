251a88afe75308a1aae854008e8c535e707eefaa
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-07-14 22:35:38 +0545

    fix: seprate the env file for url

--- frontend/src/context/GlobalContext.js
+++ frontend/src/context/GlobalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+
 
 const GlobalContext = React.createContext();
 
@@ -11,7 +11,7 @@ export const GlobalProvider = ({children}) => {
     const [error, setError] = useState(null);
 
     const addIncome = async (income) => {
-        const response = await axios.post(`${BASE_URL}add-income`, income)
+        const response = await axios.post(`${process.env.BASE_URL}add-income`, income)
             .catch((err) =>{
                 setError(err.response.data.message);
             });
